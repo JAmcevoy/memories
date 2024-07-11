fa4f1145b3ea79f3a3821f7c47a37599708d755e
Joshua McEvoy <joshmcevoy18@gmail.com>
2024-07-11 10:36:35 +0000

    07 Signin Form

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,5 @@
-import axios from "axios"
+import axios from "axios";
 
-axios.defaults.baseURL = 'https://drf-api-jam-b232223b79dc.herokuapp.com/'
-axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
-axios.defaults.withCredentials = true
+axios.defaults.baseURL = 'https://drf-api-v2-e30d7abd8950.herokuapp.com/';
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+axios.defaults.withCredentials = true;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,33 +1,10 @@
-import React, { useContext } from "react";
+import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo.png";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
-import { CurrentUserContext } from "../App";
 
 const NavBar = () => {
-  const currentUser = useContext(CurrentUserContext);
-
-  const loggedInIcons = <>{currentUser?.username}</>;
-  const loggedOutIcons = (
-    <>
-      <NavLink
-        className={styles.NavLink}
-        activeClassName={styles.Active}
-        to="/signin"
-      >
-        <i className="fas fa-sign-in-alt"></i>Sign in
-      </NavLink>
-      <NavLink
-        to="/signup"
-        className={styles.NavLink}
-        activeClassName={styles.Active}
-      >
-        <i className="fas fa-user-plus"></i>Sign up
-      </NavLink>
-    </>
-  );
-
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
@@ -36,7 +13,6 @@ const NavBar = () => {
             <img src={logo} alt="logo" height="45" />
           </Navbar.Brand>
         </NavLink>
-
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
@@ -48,8 +24,20 @@ const NavBar = () => {
             >
               <i className="fas fa-home"></i>Home
             </NavLink>
-
-            {currentUser ? loggedInIcons : loggedOutIcons}
+            <NavLink
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/signin"
+            >
+              <i className="fas fa-sign-in-alt"></i>Sign in
+            </NavLink>
+            <NavLink
+              to="/signup"
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+            >
+              <i className="fas fa-user-plus"></i>Sign up
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
